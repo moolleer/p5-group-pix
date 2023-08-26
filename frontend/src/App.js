cd92410ae04944cbae8ef4ab2268e9f1cf871185
moolleer <m_moller@live.se>
2023-08-26 17:46:14 +0000

    feat: add navbar component and favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 
-import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>GroupPix!</h1>
+      <NavBar />
     </div>
   );
 }
