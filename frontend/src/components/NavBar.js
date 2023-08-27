0ac7f488e32db1f1cf5202fc2fd3dcaa6e82c26a
moolleer <m_moller@live.se>
2023-08-27 08:57:27 +0000

    feat: setup navlinks

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,27 +1,37 @@
 import React from 'react'
-import { Navbar, Container, Nav } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+
 import logo from "../assets/logo.png"
+import styles from "../styles/NavBar.module.css"
+import { NavLink } from "react-router-dom";
 
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar expand="md" fixed="sticky-top" className={styles.CustomNavbar} >
       <Container>
         <Navbar.Brand>
-          <img src={logo} alt="logo" height="120" />
+          <img className={styles.Round} src={logo} alt="logo" height="120" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-center">
-            <Nav.Link>
+            <NavLink 
+              exact
+              to="/" 
+              className={styles.NavLink} 
+              activeClassName={styles.Active} 
+            >
               Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink >
+            <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
               Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
               Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
